fd7f1bf3395be2fe5658fa01d2c900a0cc608f76
Ryan Taylor Long <ryan@rtlong.com>
2013-06-06 16:22:52 -0700

    Update installer

--- install.zsh
+++ install.zsh
@@ -1,4 +1,26 @@
 #!/bin/zsh
 
 DOTFILES_PATH=${DOTFILES_PATH:-"$HOME/.dotfiles"}
-[[ -e "$HOME/.gitconfig" ]] || ln -s "$DOTFILES_PATH/git/gitconfig" "$HOME/.gitconfig"
+echo "Installing to $DOTFILES_PATH..."
+
+function create_link() {
+	[[ -e "$DOTFILES_PATH/$1" ]] || return
+	echo -n "  ~/$2 : "
+	if [[ ! -e "$HOME/$2" ]]; then
+		ln -sv "$DOTFILES_PATH/$1" "$HOME/$2"
+	else
+		echo "already exists"
+	fi
+}
+
+create_link 'git/gitconfig' '.gitconfig'
+create_link 'git/gitignore' '.gitignore'
+create_link 'vim/vimrc' '.vimrc'
+create_link 'ruby/irbrc' '.irbrc'
+create_link 'ruby/gemrc' '.gemrc'
+create_link 'ruby/pryrc' '.pryrc'
+
+if [[ ! -e "$HOME/.vim/bundle/vundle" ]]; then
+	git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle && \
+		vim +BundleInstall +qall
+fi
