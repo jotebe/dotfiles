a028881a72b0a5d77a593f009115bc90db365451
Ryan Taylor Long <ryan@rtlong.com>
2013-06-07 00:32:02 -0700

    Update install

--- install.zsh
+++ install.zsh
@@ -15,11 +15,23 @@ function create_link() {
 
 create_link 'git/gitconfig' '.gitconfig'
 create_link 'git/gitignore' '.gitignore'
-create_link 'vim/vimrc' '.vimrc'
+
 create_link 'ruby/irbrc' '.irbrc'
 create_link 'ruby/gemrc' '.gemrc'
 create_link 'ruby/pryrc' '.pryrc'
+create_link 'ruby/powconfig' '.powconfig'
 
+create_link 'tmux/tmux.conf' '.tmux.conf'
+
+create_link 'ackrc' '.ackrc'
+
+create_link 'zsh/prezto' '.zprezto'
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  [[ -e "${ZDOTDIR:-$HOME}/.${rcfile:t}" ]] || ln -sv "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+create_link 'vim/vimrc' '.vimrc'
 if [[ ! -e "$HOME/.vim/bundle/vundle" ]]; then
 	git clone https://github.com/gmarik/vundle.git $HOME/.vim/bundle/vundle && \
 		vim +BundleInstall +qall
