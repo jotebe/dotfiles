f0bc6d8781f86485289a8706f1d23b6f7d5898c4
Ryan Taylor Long <ryan@rtlong.com>
2015-01-27 14:53:11 -0800

    Fix an issue with setup.sh

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@
 # vim: set noexpandtab tabstop=4 shiftwidth=0:
 set -e -u
 
-if [[ -d ${HOME}/.vim/bundle/ctrlp-cmatcher ]]; then
+if [[ -d ${DOTFILES_PATH}/vim/bundle/ctrlp-cmatcher ]]; then
 	echo "Compiling C ext for ctrlp-cmatcher"
-	pushd ${HOME}/.vim/bundle/ctrlp-cmatcher
+	pushd ${DOTFILES_PATH}/vim/bundle/ctrlp-cmatcher
 	./install.sh
 	popd
 fi
